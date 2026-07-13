1984ee55197bf33dc83f4ee7a13d5d70facecaec
dotpin <d.rojas.asenjo@gmail.com>
2026-07-13 19:56:20 -0400

    New test from HomePage

---
+++ src/heroes/pages/home/HomePage.test.tsx
@@ -0,0 +1,41 @@
+import { describe, expect, test, vi } from "vitest";
+import { HomePage } from "./HomePage";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+import { usePaginatedHero } from "@/heroes/hooks/usePaginatedHero";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+vi.mock('@/heroes/hooks/usePaginatedHero')
+
+const mockUsePaginatedHero = vi.mocked(usePaginatedHero);
+
+mockUsePaginatedHero.mockReturnValue({
+    data: [],
+    isLoading: false,
+    isError: false,
+    isSuccess: true
+} as unknown as ReturnType<typeof usePaginatedHero>);
+
+const queryClient = new QueryClient();
+
+const renderHomePage = (initialEntries: string[] = ['/']) => {
+    return render(
+        <MemoryRouter initialEntries={initialEntries}>
+            <QueryClientProvider client={queryClient}>
+                <HomePage />
+            </QueryClientProvider>
+        </MemoryRouter>
+    );
+};
+
+
+describe('HomePage', () => {
+
+    test('should render HomePage with default values', () => {
+        const { container } = renderHomePage();
+
+        expect(container).toMatchSnapshot();
+    });
+
+
+});
