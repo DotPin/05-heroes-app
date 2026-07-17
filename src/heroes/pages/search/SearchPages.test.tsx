fe7d8738fe0e218c3b17f771be8862c34433477b
dotpin <d.rojas.asenjo@gmail.com>
2026-07-16 22:16:38 -0400

    Last test for SearchPages

--- src/heroes/pages/search/SearchPages.test.tsx
+++ src/heroes/pages/search/SearchPages.test.tsx
@@ -1,16 +1,29 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
-import { describe, expect, test, vi } from "vitest";
+import { beforeEach, describe, expect, test, vi } from "vitest";
 import SearchPages from "./SearchPages";
 import { searchHeroesAction } from "@/heroes/actions/search-heros.action";
+import type { Hero } from "@/heroes/types/hero.interface";
 
 vi.mock('@/heroes/actions/search-heros.action')
 const mockSearchHeroesAction = vi.mocked(searchHeroesAction);
 
 vi.mock('@/components/custom/CustomJumBotron', () => ({
-    CustomJumBotron: () => <div data-testid='custom-jumbotrom' />
-}))
+    CustomJumBotron: () => <div data-testid='custom-jumbotrom' ></div>
+}));
+
+vi.mock('@/heroes/components/HeroGrid', () => ({
+    HeroGrid: ({ heroProps }: { heroProps: Hero[] }) => (
+        <div data-testid="hero-grid">
+            {
+                heroProps.map((hero) => (
+                    <div key={hero.id}>{hero.name}</div>
+                ))
+            }
+        </div>
+    ),
+}));
 
 const queryClient = new QueryClient();
 
@@ -21,27 +34,89 @@ const renderSearchPage = (initialEntries: string[] = ['/']) => {
                 <SearchPages />
             </QueryClientProvider>
         </MemoryRouter>
-    )
-}
-
+    );
+};
 
 describe('SearchPageTest', () => {
 
-    test('should render SearchPage with default valius ', () => {
+    beforeEach(() => {
+        vi.clearAllMocks();
+    })
+
+    test('should render SearchPage with default values ', () => {
 
         const { containter } = renderSearchPage();
 
-        //renderSearchPage();
+        expect(mockSearchHeroesAction).toHaveBeenCalledWith({
+            name: "",
+            strength: NaN,
+        });
+
+        expect(containter).toMatchSnapshot();
+    });
+
+    test('should call search action with name parameter', () => {
+
+        const { containter } = renderSearchPage(['/search?name=superman']);
 
         expect(mockSearchHeroesAction).toHaveBeenCalledWith({
-            name: undefined,
+            name: 'superman',
             strength: NaN,
         });
 
         expect(containter).toMatchSnapshot();
 
-        //screen.debug();
+
     });
 
+    test('should call search action with strength parameter', () => {
 
-})
+        const { containter } = renderSearchPage(['/search?strength=10']);
+
+        expect(mockSearchHeroesAction).toHaveBeenCalledWith({
+            name: "",
+            strength: 10,
+        });
+
+        expect(containter).toMatchSnapshot();
+
+
+    });
+
+    test('should call search action with strength and name parameter', () => {
+
+        const { containter } = renderSearchPage(['/search?name=batman&strength=6']);
+
+        expect(mockSearchHeroesAction).toHaveBeenCalledWith({
+            name: 'batman',
+            strength: 6,
+        });
+
+        expect(containter).toMatchSnapshot();
+
+    });
+
+    test('should render HeroGrid with search result', async () => {
+
+        //enviamos Query parameters
+        const mockHeroes = [
+            { id: '1', name: 'Clark Kent' } as unknown as Hero,
+            { id: '2', name: 'Bruce Wayne' } as unknown as Hero,
+        ]
+
+        mockSearchHeroesAction.mockResolvedValue(mockHeroes);
+
+        renderSearchPage();
+
+        await waitFor(() => {
+            expect(screen.getByText('Clark Kent')).toBeDefined();
+            expect(screen.getByText('Bruce Wayne')).toBeDefined()
+            //permite mostrar componente cual se desea probar
+            //screen.debug([
+            //    screen.getByTestId('hero-grid'),
+            //]);
+        })
+
+    })
+
+});
